fd50163ebcdb307c054dc00659b4dae98fe09500
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-12 13:16:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ header_col1, header_col2, header_col3 = st.columns([10, 1.7, 3.3])
 
 with header_col1:
   st.title('beat inspector ™')
-  st.write('by rs. analytics [https://github.com/stefanrmmr] - version 1.0.0 April 2022')
+  st.write('by rs. analytics (github @stefanrmmr) - version 1.0.0 April 2022')
   st.write('')
 with header_col3:
   st.image("rs_logo_transparent.png")
