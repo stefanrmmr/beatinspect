800376609386e7bfee5f52570411be223d86e370
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-12 14:44:40 +0200

    add design

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,37 @@ import toml
 
 # primaryColor = toml.load("config.toml")['theme']['primaryColor']
 # primaryColor = st.get_option("theme.primaryColor")
-s = f"""
-<style>
-div.stButton > button:first-child {background-color: #0099ff; color:#ffffff;}
-div.stButton > button:hover {background-color: #00ff00; color:#ff0000;}
-<style>
-"""
-st.markdown(s, unsafe_allow_html=True)
+# # s = f"""
+# <style>
+# div.stButton > button:first-child {{ border: 5px solid {primaryColor}; border-radius:20px 20px 20px 20px; }}
+# <style>
+# """
+# st.markdown(s, unsafe_allow_html=True)
 
-
-
-
+st.markdown("""
+div.stButton > button:first-child {
+background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
+}
+.css-2trqyj:focus:not(:active) {
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+.css-2trqyj:focus:(:active) {
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+.css-2trqyj:focus:active){
+background-color: #0066cc;
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+“”", unsafe_allow_html=True)
 
 
 # Streamlit Design Choices (remove red header line)
