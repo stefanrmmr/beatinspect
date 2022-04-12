d4b4e788b4feb5dedff4d0eb83a5da5ef09b8f1f
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-12 17:19:40 +0200

    clean code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,17 @@
 # beat_inspector by stefanrmmr (rs. analytics) - version April 2022
-
 import streamlit as st
 import toml
 
+# Streamlit Design Choices (page layout)
 st.set_page_config(layout="centered",
     page_icon="resources/rs_logo_transparent.png",
     page_title="beat inspector")
 
-# Streamlit Design Choices (remove red header line)
 hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
 st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
-# TITLE and information
+# Title and Information
 header_col1, header_col2, header_col3 = st.columns([10, 1.7, 3.3])
-
 with header_col1:
     st.title('beat inspector ™')
     st.write('by rs. analytics (github @stefanrmmr) - version 1.0.0 April 2022')
@@ -21,11 +19,9 @@ with header_col1:
 with header_col3:
     st.image("resources/rs_logo_transparent.png")
 
-
-if st.button('servus'):
-    st.write('hello')
-
-audiofile_upload = st.file_uploader("Please select and upload an audio file (.wav)",type='wav')
+# Audio File Upload
+audiofile_upload = st.
+    file_uploader("Please select and upload an audio file (.wav)",type='wav')
 # Set Preferences & Key
 if audiofile_upload is not None:
     st.write('file upload complete')
