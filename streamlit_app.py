30694fc1eaf5f73c2b9dbb7f07fc1fa7d21ffd83
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-12 13:11:24 +0200

    Update streamlit_app.py
    change logo

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,14 @@ hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
 st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
 # TITLE and information 
-header_col1, header_col2, header_col3 = st.columns([10, 1, 4])
+header_col1, header_col2, header_col3 = st.columns([10, 3, 2])
 
 with header_col1:
   st.title('beat inspector™')
   st.markdown('by [rs. analytics](https://www.linkedin.com/in/stefanrmmr/) - version 1.0.0 April 2022')
   st.write('')
 with header_col3:
-  st.image("rs_logo.png")
+  st.image("rs_logo_transparent.png")
 
 
 
