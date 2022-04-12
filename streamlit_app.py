60fe4a23b382dfe02ddbcae7d32ef5db76f009f3
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-12 14:00:07 +0200

    add stylesheets css theme

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,30 @@
 # beat_inspector by stefanrmmr (rs. analytics) - version April 2022
 
 import streamlit as st
+st.markdown("""
+div.stButton > button:first-child {
+background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
+}
+.css-2trqyj:focus:not(:active) {
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+.css-2trqyj:focus:(:active) {
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+.css-2trqyj:focus:active){
+background-color: #0066cc;
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+""", unsafe_allow_html=True)
 
 # Streamlit Design Choices (remove red header line)
 hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
