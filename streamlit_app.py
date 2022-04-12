48d96924fde9ee65c3cd5ad81d7bf92e4f8f1799
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-13 01:59:38 +0200

    expander columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,12 @@ with header_col3:
 
 # Audio File Upload
 with st.expander("Please select and upload an audio file (.wav)",expanded=True):
-    audiofile_upload = st.file_uploader("", type='wav')
-    st.audio(audiofile_upload)  # display audio player UX
+
+    exp_col1, exp_col2 = st.columns([1,1])
+    with exp_col1:
+        audiofile_upload = st.file_uploader("", type='wav')
+    with exp_col2:
+        st.audio(audiofile_upload)  # display audio player UX
 
 # Set Preferences for Analytics
 if audiofile_upload is not None:
