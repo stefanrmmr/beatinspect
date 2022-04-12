6a8b84d2c3c42bbc07f11066dbc83704ff736e9f
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-12 16:34:22 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ div.stButton > button:first-child {border: 5px solid {primaryColor}; border-radi
 """
 st.markdown(s, unsafe_allow_html=True)
 
-background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
+# background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
 
 
 # Streamlit Design Choices (remove red header line)
