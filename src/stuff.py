c88896c9627aa8e909a6e11a22cfb4e80136e8d4
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-12 14:57:43 +0200

    add design

---
+++ src/stuff.py
@@ -0,0 +1,25 @@
+# sdsdfsdfsf
+"""st.markdown('''
+<style>
+div.stButton > button:first-child {
+background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
+}
+.css-2trqyj:focus:not(:active) {
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+.css-2trqyj:focus:(:active) {
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}
+.css-2trqyj:focus:active){
+background-color: #0066cc;
+border-color: #ffffff;
+box-shadow: none;
+color: #ffffff;
+background-color: #0066cc;
+}<style>''', unsafe_allow_html=True)"""
--- streamlit_app.py
+++ streamlit_app.py
@@ -12,35 +12,10 @@ import toml
 # """
 # st.markdown(s, unsafe_allow_html=True)
 
-"""st.markdown('''
-<style>
-div.stButton > button:first-child {
-background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
-}
-.css-2trqyj:focus:not(:active) {
-border-color: #ffffff;
-box-shadow: none;
-color: #ffffff;
-background-color: #0066cc;
-}
-.css-2trqyj:focus:(:active) {
-border-color: #ffffff;
-box-shadow: none;
-color: #ffffff;
-background-color: #0066cc;
-}
-.css-2trqyj:focus:active){
-background-color: #0066cc;
-border-color: #ffffff;
-box-shadow: none;
-color: #ffffff;
-background-color: #0066cc;
-}<style>''', unsafe_allow_html=True)"""
-
 
 # Streamlit Design Choices (remove red header line)
-hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
-st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+# hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
+# st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
 
 # TITLE and information
