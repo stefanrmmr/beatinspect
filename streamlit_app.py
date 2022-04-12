2524a87cc0f19d365c53c932558a78e539a89acd
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-12 14:58:57 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@
 import streamlit as st
 import toml
 
-# primaryColor = toml.load("config.toml")['theme']['primaryColor']
+primaryColor = toml.load(".streamlit/config.toml")['theme']['primaryColor']
 # primaryColor = st.get_option("theme.primaryColor")
 # # s = f"""
 # <style>
@@ -14,9 +14,8 @@ import toml
 
 
 # Streamlit Design Choices (remove red header line)
-# hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
-# st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
-
+hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
+st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
 # TITLE and information
 header_col1, header_col2, header_col3 = st.columns([10, 1.7, 3.3])
