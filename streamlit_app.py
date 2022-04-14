f17c2bf370c4c1a3fc9f8b2d3a8be8809e79f74f
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-14 19:45:59 +0200

    powered by

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,3 +77,6 @@ if audiofile is not None:
             st.metric(label="Audio File Technical Specifications", value=f"{round(bpm_essentia, 1)} BPM", delta=f'{channels} - WAV {sampling_freq} Hz', delta_color="off")
             bpm_output = f'<p style="font-family:sans-serif; color:{primary_color}; font-size: 25.6px;">Musical Scale (SOON!)</p>'
             st.markdown(bpm_output, unsafe_allow_html=True)
+
+
+st.write('powered by upf.\'s')
