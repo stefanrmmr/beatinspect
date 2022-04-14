ecebaa659ac5d47a1445a4bf2f072664629401a9
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-14 19:25:34 +0200

    reset ech specs expander

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,40 +49,38 @@ if audiofile is not None:
         st.audio(audiofile)  # display audio player UX
 
     # Musical and Tech Specs Overview
-    with st.expander("SECTION - Musical & Technical Specifications", expanded=True):
+    # with st.expander("SECTION - Musical & Technical Specifications", expanded=True):
 
-        tech_col1, tech_col2, tech_col3 = st.columns([1,1,1])
 
-        with tech_col1:
-            st.write('')
-            st.write('')
-            st.write('')
-            st.write('')
-            if st.button('Analyze Audio'):
-                with tech_col3:
-                    with st.spinner('Calculating BPM'):
-                        # extract tech Specifications about wav file
-                        sampling_freq, channels = wav_techspecs.read_wav(audiofile)
-                        # BPM estimation powered by essentia library
-                        es_audio = es.MonoLoader(filename=audiofile.name)()
-                        rhythm_extractor = es.RhythmExtractor2013(method="multifeature")
-                        bpm_essentia, es_beats, beats_confidence, _, beats_intervals = rhythm_extractor(es_audio)
 
-                        # channel information description
-                        if int(channels) == 1:  # single channel .wav
-                            channels = 'Mono'
-                        elif int(channels) == 2:  # double channel .wav
-                            channels = 'Stereo'
-                        else:  # multi channel .wav
-                            channels = str(channels) + ' Channel Audio'
+    st.write('')  # add spacing
+    pref_col1, pref_col2, pref_col3 = st.columns([8, 5, 8])
 
-                with tech_col2:
-                    st.metric(label="", value=f"{round(bpm_essentia, 1)} BPM", delta=f'{channels} - WAV {sampling_freq} Hz', delta_color="off")
-                    bpm_output = f'<p style="font-family:sans-serif; color:{primary_color}; font-size: 25.6px;">Musical Scale (SOON!)</p>'
-                    st.markdown(bpm_output, unsafe_allow_html=True)
 
-foot_col1, foot_col2, foot_col3 = st.columns([2, 1, 10])
-with foot_col1:
-    st.write('powered by')
-with foot_col2:
-    st.image('resources/essentia_logo.png')
+    with pref_col2:
+        # Initiate Analysis of bpm
+        st.write('')  # add spacing
+        st.write('')  # add spacing
+        if st.button('Start Analysis'):
+            with pref_col3:
+                with st.spinner('Calculating BPM'):
+
+                    # extract tech Specifications about wav file
+                    sampling_freq, channels = wav_techspecs.read_wav(audiofile)
+
+                    # BPM estimation using essentia library
+                    es_audio = es.MonoLoader(filename=audiofile.name)()
+                    rhythm_extractor = es.RhythmExtractor2013(method="multifeature")
+                    bpm_essentia, es_beats, beats_confidence, _, beats_intervals = rhythm_extractor(es_audio)
+
+                    if int(channels) == 1:  # single channel .wav
+                        channels = 'Mono'
+                    elif int(channels) == 2:  # double channel .wav
+                        channels = 'Stereo'
+                    else:  # multi channel .wav
+                        channels = str(channels) + ' Channel Audio'
+
+            with pref_col3:  # Output Analytics Results
+                st.metric(label="Audio File Technical Specifications", value=f"{round(bpm_essentia, 1)} BPM", delta=f'{channels} - WAV {sampling_freq} Hz', delta_color="off")
+                bpm_output = f'<p style="font-family:sans-serif; color:{primary_color}; font-size: 25.6px;">Musical Scale (SOON!)</p>'
+                st.markdown(bpm_output, unsafe_allow_html=True)
