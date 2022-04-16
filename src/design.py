66320dc6151348009b9c04faebc327f2816a3be7
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-16 21:43:13 +0200

    hide streamlit footer hide streamlit footer

--- src/design.py
+++ src/design.py
@@ -14,9 +14,11 @@ def design_setup():
     hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
     st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
-    # Design hide "made with streamlit" footer menu area 
-    hide_streamlit_footer = """<style>#MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}</style>"""
+    # Design hide "made with streamlit" footer menu area
+    hide_streamlit_footer = """<style>
+                            #MainMenu {visibility: hidden;}
+                            footer {visibility: hidden;}
+                            </style>"""
     st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
 
     # Design change spinner color to primary color
