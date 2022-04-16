9b88dd63fb6e08afa8bf741596247350ae484e66
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-16 14:13:10 +0200

    add main function structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,104 +34,110 @@ st.markdown('''<style>.st-db {width: 8px;}</style>''',
 st.markdown('''<style>.st-da {height: 8px;}</style>''',
     unsafe_allow_html=True)
 
-# Title and Information
-header_col1, header_col2, header_col3 = st.columns([10, 2.5, 2.5])
-with header_col1:
-    st.title('beat inspect ™')
-    st.write('Version 1.2.0 - April 2022 - Github @stefanrmmr')
-with header_col3:
-    st.write('')  # add spacing
-    st.image("resources/rs_logo_transparent_yellow.png")
 
-# Audio File Upload
-with st.expander("SECTION - Select prefered Audio Input Option",expanded=True):
+def beatinspect_main():
 
-    audio_col0, audio_col1, audio_col2 = st.columns([0.03,0.5,1])
-    with audio_col1:
-        choice = st.radio('',[' Audio File Upload', ' Record via Microphone', ' Read from Audio Chip'])
+    # Title and Information
+    header_col1, header_col2, header_col3 = st.columns([10, 2.5, 2.5])
+    with header_col1:
+        st.title('beat inspect ™')
+        st.write('Version 1.2.0 - April 2022 - Github @stefanrmmr')
+    with header_col3:
         st.write('')  # add spacing
+        st.image("resources/rs_logo_transparent_yellow.png")
 
-    with audio_col2:
-        if 'Upload' in choice:
-            audiofile = st.file_uploader("", type='wav')
-        elif 'Record' in choice:
-            st.write('SOON1')  # TODO SOON
-            audiofile = None
-            # https://github.com/ayushkumarshah/Guitar-Chords-recognition/blob/master/src/sound.py
-        else:
-            st.write('SOON2')  # TODO SOON
-            audiofile = None
+    # Audio File Upload
+    with st.expander("SECTION - Select prefered Audio Input Option",expanded=True):
 
-# Audio File Analytics
-if audiofile is not None:
-
-    # Save audiofile to main directory to be called via path
-    with open(audiofile.name,"wb") as f:
-        f.write(audiofile.getbuffer())
-
-    # Inspect Audio File Specifications
-    with st.expander("SECTION - Waveform and Spectrogram Insights", expanded=False):
-        st.audio(audiofile)  # display audio player UX
-
-        # TODO select a section of the track (or the whole track) and analyze for sections that are above ZERO level
-        # TODO for these sections that are "übersteuern" --> find out in which frequency bands they need to be decreased in amplitude
-        # PLOT amplitude over frequency --> classical EQ view --> Spectrogram
-        # Step 1 Select slider timeframe from overall plotted audio file (AMP oder time)
-        # Step 2 Use the timeframe to calculate the Spectrogram (AMP(frequency))
-        # Step 3 Plot Spectrogram plot with yellow vertical bars at frequencies where AMP too high!
-
-    # Musical and Tech Specs Overview
-    with st.expander("SECTION - Musical & Technical Specifications", expanded=True):
-
-        pref_col0, pref_col1, pref_col2, pref_col3 = st.columns([0.2, 1, 1, 1])
-
-        with pref_col1:  # output: column for music scale evaluation
-            with st.spinner('Finding Key & Scale'):
-                time.sleep(0.5)
-                # call utility function that calculates key,scale using essentia
-                # https://essentia.upf.edu/reference/streaming_Key.html
-                key, scale, key_strength = detect_keyscale.detect_ks(audiofile.name, 'diatonic')
-
-            # scale_text = f'<p color:{primary_color}; font-size: 32px;">{key}-{scale}</p>'
-            # conf_text = f'<p  color: white; font-size: 18px;">{round(key_strength, 2)}</p>'
-            # st.markdown(scale_text, unsafe_allow_html=True)
-            # st.markdown(conf_text, unsafe_allow_html=True)
-            st.metric(label="", value=f"{key}-{scale}",
-                      delta=f"Confidence {round(key_strength, 2)}",
-                      delta_color="off")
+        audio_col0, audio_col1, audio_col2 = st.columns([0.03,0.5,1])
+        with audio_col1:
+            choice = st.radio('',[' Audio File Upload', ' Record via Microphone', ' Read from Audio Chip'])
             st.write('')  # add spacing
 
-        with pref_col2:  # metrics: generating insights on tech specs
-            with st.spinner('Fetching Tech Specs'):
-                time.sleep(0.5)
-                # extract tech Specifications about wav file
-                sampling_freq, channels = wav_techspecs.read_wav(audiofile)
-                # assign audio channel description
-                if int(channels) == 1:  # single channel .wav
-                    channels = 'MONO'
-                elif int(channels) == 2:  # double channel .wav
-                    channels = 'STEREO'
-                else:  # multi channel .wav
-                    channels = str(channels) + ' Channel'
-            st.metric(label="", value=f"{sampling_freq} Hz",
-                      delta=f'WAV - {channels}', delta_color="off")
-            st.write('')  # add spacing
+        with audio_col2:
+            if 'Upload' in choice:
+                audiofile = st.file_uploader("", type='wav')
+            elif 'Record' in choice:
+                st.write('SOON1')  # TODO SOON
+                audiofile = None
+                # https://github.com/ayushkumarshah/Guitar-Chords-recognition/blob/master/src/sound.py
+            else:
+                st.write('SOON2')  # TODO SOON
+                audiofile = None
 
-        with pref_col3:  # metrics: calculcation of tempo
-            with st.spinner('Calculating BPM'):
-                time.sleep(0.5)
-                # BPM estimation using essentia library
-                es_audio = es.MonoLoader(filename=audiofile.name)()
-                rhythm_extractor = es.RhythmExtractor2013(method="multifeature")
-                bpm_essentia, _, _, _, _ = rhythm_extractor(es_audio)
-            st.metric(label="", value=f"{round(bpm_essentia, 1)} BPM",
-                      delta=f'Beat Tempo', delta_color="off")
-            st.write('')  # add spacing
+    # Audio File Analytics
+    if audiofile is not None:
 
+        # Save audiofile to main directory to be called via path
+        with open(audiofile.name,"wb") as f:
+            f.write(audiofile.getbuffer())
 
-# FOOTER Content and Coop logos etc
-foot_col1, foot_col2, foot_col3, foot_col4 = st.columns([3,1.5,1.5,3])
-with foot_col2:
-    st.image('resources/powered_by_essentia.png')
-with foot_col3:
-    st.image('resources/coop_utility_studio.png')
+        # Inspect Audio File Specifications
+        with st.expander("SECTION - Waveform and Spectrogram Insights", expanded=False):
+            st.audio(audiofile)  # display audio player UX
+
+            # TODO select a section of the track (or the whole track) and analyze for sections that are above ZERO level
+            # TODO for these sections that are "übersteuern" --> find out in which frequency bands they need to be decreased in amplitude
+            # PLOT amplitude over frequency --> classical EQ view --> Spectrogram
+            # Step 1 Select slider timeframe from overall plotted audio file (AMP oder time)
+            # Step 2 Use the timeframe to calculate the Spectrogram (AMP(frequency))
+            # Step 3 Plot Spectrogram plot with yellow vertical bars at frequencies where AMP too high!
+
+        # Musical and Tech Specs Overview
+        with st.expander("SECTION - Musical & Technical Specifications", expanded=True):
+
+            pref_col0, pref_col1, pref_col2, pref_col3 = st.columns([0.2, 1, 1, 1])
+
+            with pref_col1:  # output: column for music scale evaluation
+                with st.spinner('Finding Key & Scale'):
+                    time.sleep(0.5)
+                    # call utility function that calculates key,scale using essentia
+                    # https://essentia.upf.edu/reference/streaming_Key.html
+                    key, scale, key_strength = detect_keyscale.detect_ks(audiofile.name, 'diatonic')
+
+                # scale_text = f'<p color:{primary_color}; font-size: 32px;">{key}-{scale}</p>'
+                # conf_text = f'<p  color: white; font-size: 18px;">{round(key_strength, 2)}</p>'
+                # st.markdown(scale_text, unsafe_allow_html=True)
+                # st.markdown(conf_text, unsafe_allow_html=True)
+                st.metric(label="", value=f"{key}-{scale}",
+                          delta=f"Confidence {round(key_strength, 2)}",
+                          delta_color="off")
+                st.write('')  # add spacing
+
+            with pref_col2:  # metrics: generating insights on tech specs
+                with st.spinner('Fetching Tech Specs'):
+                    time.sleep(0.5)
+                    # extract tech Specifications about wav file
+                    sampling_freq, channels = wav_techspecs.read_wav(audiofile)
+                    # assign audio channel description
+                    if int(channels) == 1:  # single channel .wav
+                        channels = 'MONO'
+                    elif int(channels) == 2:  # double channel .wav
+                        channels = 'STEREO'
+                    else:  # multi channel .wav
+                        channels = str(channels) + ' Channel'
+                st.metric(label="", value=f"{sampling_freq} Hz",
+                          delta=f'WAV - {channels}', delta_color="off")
+                st.write('')  # add spacing
+
+            with pref_col3:  # metrics: calculcation of tempo
+                with st.spinner('Calculating BPM'):
+                    time.sleep(0.5)
+                    # BPM estimation using essentia library
+                    es_audio = es.MonoLoader(filename=audiofile.name)()
+                    rhythm_extractor = es.RhythmExtractor2013(method="multifeature")
+                    bpm_essentia, _, _, _, _ = rhythm_extractor(es_audio)
+                st.metric(label="", value=f"{round(bpm_essentia, 1)} BPM",
+                          delta=f'Beat Tempo', delta_color="off")
+                st.write('')  # add spacing
+
+    # FOOTER Content and Coop logos etc
+    foot_col1, foot_col2, foot_col3, foot_col4 = st.columns([3,1.5,1.5,3])
+    with foot_col2:
+        st.image('resources/powered_by_essentia.png')
+    with foot_col3:
+        st.image('resources/coop_utility_studio.png')
+
+if __name__ == '__main__':
+    # call main function
+    beatinspect_main()
