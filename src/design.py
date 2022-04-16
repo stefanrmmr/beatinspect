36e53f3b067131bc85fd486fa66bd4ec316126d6
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-16 21:50:33 +0200

    custom footer

--- src/design.py
+++ src/design.py
@@ -24,10 +24,10 @@ def design_setup():
 
 
 
-    footer="""<style> your css code put here</style>
-            <div class='footer'>
+    footer="""<div class='footer'>
             <p>the word you want to tell<a style='display:block;text-align:center;'
-            href='https://www.streamlit.io' target='_blank'>your email address put here</a></p>
+            href='https://www.streamlit.io'
+            target='stefan.rummer@outlook.com'>your email address put here</a></p>
             </div>"""
     st.markdown(footer, unsafe_allow_html=True)
 
