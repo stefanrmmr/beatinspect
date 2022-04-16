968e366d6c99b9feccfe16c8b6a550b52af46fee
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-16 14:21:52 +0200

    change img dim

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ def get_img_with_href(local_img_path, target_url):
     bin_str = get_base64_of_bin_file(local_img_path)
     html_code = f'''
         <a href="{target_url}">
-            <img src="data:image/{img_format};base64,{bin_str}" width="200" height="100" />
+            <img src="data:image/{img_format};base64,{bin_str}" width="100" height="50" />
         </a>'''
     return html_code
 
