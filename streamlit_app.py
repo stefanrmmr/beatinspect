1937a35cc79858b4649cf2a840f33f5865cabdd7
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-17 06:09:28 +0200

    overlay

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,11 +119,13 @@ def beatinspect_main():
 
 
 
-            #librosa.display.waveshow(y, sr, ax=ax1, color='grey', x_axis='time')
+
 
             # img2 = librosa.display.specshow(scale_db, ax=ax2, x_axis='time', y_axis='linear')
-            img2 = librosa.display.specshow(scale_db, ax=ax1, sr=sr, x_axis='time', y_axis='linear')
-            fig.colorbar(img2, ax=ax1, format="%+2.f dB")
+            librosa.display.specshow(scale_db, ax=ax1, sr=sr, x_axis='time', y_axis='linear')
+
+            librosa.display.waveshow(y, sr, ax=ax1, color='grey', x_axis='time')
+            # fig.colorbar(img2, ax=ax1, format="%+2.f dB")
 
             # plt.xlabel('')
             st.pyplot(fig)
