53392e03bc97c1c554e8df3e21af6343c2baf966
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-17 02:45:01 +0200

    test existing spectrogram function

--- helper.py
+++ helper.py
@@ -1,37 +1,4 @@
-import matplotlib.pyplot as plt
-import librosa
-from pathlib import Path
-import sounddevice as sd
-import wavio
 
-
-def create_spectrogram(voice_sample):
-    """
-    Creates and saves a spectrogram plot for a sound sample.
-    Parameters:
-        voice_sample (str): path to sample of sound
-    Return:
-        fig
-    """
-
-    in_fpath = Path(voice_sample.replace('"', "").replace("'", ""))
-    original_wav, sampling_rate = librosa.load(str(in_fpath))
-
-    # Plot the signal read from wav file
-    fig = plt.figure()
-    plt.subplot(211)
-    plt.title(f"Spectrogram of file {voice_sample}")
-
-    plt.plot(original_wav)
-    plt.xlabel("Sample")
-    plt.ylabel("Amplitude")
-
-    plt.subplot(212)
-    plt.specgram(original_wav, Fs=sampling_rate)
-    plt.xlabel("Time")
-    plt.ylabel("Frequency")
-    # plt.savefig(voice_sample.split(".")[0] + "_spectogram.png")
-    return fig
 
 def read_audio(file):
     with open(file, "rb") as audio_file:
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,12 @@ import streamlit as st
 import base64
 import os
 
+import matplotlib.pyplot as plt
+import librosa
+from pathlib import Path
+import wavio
+
+
 # UTILITY enable displaying images with hrefs
 @st.cache(allow_output_mutation=True)
 def get_base64_of_bin_file(bin_file):
@@ -18,3 +24,33 @@ def get_img_with_href(local_img_path, target_url):
             <img src="data:image/{img_format};base64,{bin_str}" width="100%" height="100%" />
         </a>'''
     return html_code
+
+
+# PLOTTING Spectrogram analytics
+def create_spectrogram(voice_sample):
+    """
+    Creates and saves a spectrogram plot for a sound sample.
+    Parameters:
+        voice_sample (str): path to sample of sound
+    Return:
+        fig
+    """
+
+    in_fpath = Path(voice_sample.replace('"', "").replace("'", ""))
+    original_wav, sampling_rate = librosa.load(str(in_fpath))
+    
+    # Plot the signal read from wav file
+    fig = plt.figure()
+    plt.subplot(211)
+    plt.title(f"Spectrogram of file {voice_sample}")
+
+    plt.plot(original_wav)
+    plt.xlabel("Sample")
+    plt.ylabel("Amplitude")
+
+    plt.subplot(212)
+    plt.specgram(original_wav, Fs=sampling_rate)
+    plt.xlabel("Time")
+    plt.ylabel("Frequency")
+    # plt.savefig(voice_sample.split(".")[0] + "_spectogram.png")
+    return fig
