d14833564baa46e63f71c99f2644e6304b3b85c3
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-17 16:47:21 +0200

    change label to top

--- src/plots.py
+++ src/plots.py
@@ -29,7 +29,7 @@ def rms_spectrum(times, rms):
     ax2.patch.set_alpha(0.0)
     ax2.set_ylabel('RMS Energy [log]')
     ax2.set_xlabel('Time [sec]')
-    ax2.xaxis.set_ticks_position('top') # the rest is the same
+    # ax2.xaxis.set_ticks_position('top') # the rest is the same
     # ax2.get_xaxis().set_visible(False)
     ax2.set_ylim(bottom=0.0001)                 # setting lower bounds for y axis
     ax2.xaxis.label.set_color('white')        #setting up X-axis label color to yellow
@@ -41,7 +41,7 @@ def rms_spectrum(times, rms):
     ax2.spines['right'].set_color('white')        # setting up Y-axis tick color to red
     ax2.spines['bottom'].set_color('white')         #setting up above X-axis tick color to red
     ax2.spines['right'].set_visible(False)      # Hide the right and top spines
-    ax2.spines['bottom'].set_visible(False)     # Hide the right and bottom spines
+    # ax2.spines['bottom'].set_visible(False)     # Hide the right and bottom spines
 
     plt.tight_layout()
     st.pyplot(fig)
