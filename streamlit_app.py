f4f3e1ccb5d4e3954bd12391d658ab3d72f4df4f
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-17 16:22:56 +0200

    bugfix

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,7 +102,7 @@ def beatinspect_main():
 
             # radio button selection for spectrum plot over time
             st.write('<style>div.row-widget.stRadio > div{flex-direction:row;} </style>', unsafe_allow_html=True)
-            #st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{padding-left:2px;}</style>', unsafe_allow_html=True)
+            st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{padding-left:2px;}</style>', unsafe_allow_html=True)
 
             sradio_col1, sradio_col2 = st.columns([0.03, 1.5])
             with sradio_col2:
