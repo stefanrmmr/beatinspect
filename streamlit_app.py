995e3873d3e0d02053f488654119dc1088abe506
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-17 02:23:51 +0200

    recover state and add knowledge notes

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,153 +11,11 @@ import src.utils as utils  # utility functions
 import src.design as design  # design choices
 import src.wav_techspecs as wav_techspecs
 import src.detect_keyscale as detect_keyscale
-
-
 import streamlit.components.v1 as components
 
 def beatinspect_main():
     # DESIGN implement changes to the standard streamlit UI/UX
     design.design_setup()  # switch to primaryColor for accents
-
-
-
-    main_html = """
-    <html>
-      <head>
-        <script>
-            let log = console.log.bind(console),
-              id = val => document.getElementById(val),
-              ul = id('ul'),
-              gUMbtn = id('gUMbtn'),
-              start = id('start'),
-              stop = id('stop'),
-              stream,
-              recorder,
-              counter=1,
-              chunks,
-              media;
-
-
-            gUMbtn.onclick = e => {
-              let mv = id('mediaVideo'),
-                  mediaOptions = {
-                    video: {
-                      tag: 'video',
-                      type: 'video/webm',
-                      ext: '.mp4',
-                      gUM: {video: true, audio: true}
-                    },
-                    audio: {
-                      tag: 'audio',
-                      type: 'audio/ogg',
-                      ext: '.ogg',
-                      gUM: {audio: true}
-                    }
-                  };
-              media = mv.checked ? mediaOptions.video : mediaOptions.audio;
-              navigator.mediaDevices.getUserMedia(media.gUM).then(_stream => {
-                stream = _stream;
-                id('gUMArea').style.display = 'none';
-                id('btns').style.display = 'inherit';
-                start.removeAttribute('disabled');
-                recorder = new MediaRecorder(stream);
-                recorder.ondataavailable = e => {
-                  chunks.push(e.data);
-                  if(recorder.state == 'inactive')  makeLink();
-                };
-                log('got media successfully');
-              }).catch(log);
-            }
-
-            start.onclick = e => {
-              start.disabled = true;
-              stop.removeAttribute('disabled');
-              chunks=[];
-              recorder.start();
-            }
-
-
-            stop.onclick = e => {
-              stop.disabled = true;
-              recorder.stop();
-              start.removeAttribute('disabled');
-            }
-
-
-
-            function makeLink(){
-              let blob = new Blob(chunks, {type: media.type })
-                , url = URL.createObjectURL(blob)
-                , li = document.createElement('li')
-                , mt = document.createElement(media.tag)
-                , hf = document.createElement('a')
-              ;
-              mt.controls = true;
-              mt.src = url;
-              hf.href = url;
-              hf.download = `${counter++}${media.ext}`;
-              hf.innerHTML = `donwload ${hf.download}`;
-              li.appendChild(mt);
-              li.appendChild(hf);
-              ul.appendChild(li);
-            }
-
-        </script>
-        <title>MediaRecorder API - Sample</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="keywords" content="WebRTC getUserMedia MediaRecorder API">
-        <link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-        <style>
-          button{
-            margin: 10px 5px;
-          }
-          li{
-            margin: 10px;
-          }
-          body{
-            width: 90%;
-            height: 500px;
-            max-width: 960px;
-            margin: 0px auto;
-          }
-          #btns{
-            display: none;
-          }
-          h1{
-            margin: 100px;
-          }
-        </style>
-      </head>
-      <body>
-        <h1> MediaRecorder API example</h1>
-
-        <p> For now it is supported only in Firefox(v25+) and Chrome(v47+)</p>
-        <div id='gUMArea'>
-          <div>
-          Record:
-            <input type="radio" name="media" value="video" checked id='mediaVideo'>Video
-            <input type="radio" name="media" value="audio">audio
-          </div>
-          <button class="btn btn-default"  id='gUMbtn'>Request Stream</button>
-        </div>
-        <div id='btns'>
-          <button  class="btn btn-default" id='start'>Start</button>
-          <button  class="btn btn-default" id='stop'>Stop</button>
-        </div>
-        <div>
-          <ul  class="list-unstyled" id='ul'></ul>
-        </div>
-        <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-      </body>
-    </html>
-    """
-
-    # Execute your app
-    st.title("Javascript example")
-    components.html(main_html, height=500)  # JavaScript works
-
 
     # TITLE and Information
     header_col1, header_col2, header_col3 = st.columns([10, 2.5, 2.5])
@@ -183,7 +41,13 @@ def beatinspect_main():
                 audiofile = st.file_uploader("", type='wav')
             elif 'Record' in choice:
                 audiofile = None
-
+                # https://www.youtube.com/watch?v=BuD3gILJW-Q&ab_channel=Streamlit
+                # USE streamlit custom component that implements a custom html/css/react element
+                # input data can be passed to this component and it returns data
+                # TODO build a component that uses the html/script code of above (for demo webapp rec tool)
+                # the component should show up in case the radio button record mic is selected
+                # the component should return a path to a audiofile
+                # clean existing audio files bevore saving the new audio file! (tmp dictonary)
 
     # ANALYTICS for Audio File
     if audiofile is not None:
