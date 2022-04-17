f3d577de8c238762026335332c13610e38634352
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-17 16:11:54 +0200

    bugfix

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,22 +86,23 @@ def beatinspect_main():
             st.audio(audiofile)  # display audio player UX
 
             # display the selected spectrum plot
-            spectrum_coice = st.session_state.spectrum
-            if 'AMP' in spectrum_coice:
-                with st.spinner('generating AMP spectrum plot'):
-                    # generate amp spectrum plots
-                    # plots.amprms_spectrum(y, sr, times, rms)
-                    plots.amp_spectrum(y,sr)
-            else:
+            # spectrum_coice = st.session_state.spectrum
+            if 'RMS' in spectrum_coice:
                 with st.spinner('generating RMS spectrum plot'):
                     # generate rms spectrum plots
                     # plots.amprms_spectrum(y, sr, times, rms)
                     plots.rms_spectrum(times, rms)
+            else:
+                with st.spinner('generating AMP spectrum plot'):
+                    # generate amp spectrum plots
+                    # plots.amprms_spectrum(y, sr, times, rms)
+                    plots.amp_spectrum(y,sr)
 
             # radio button selection for spectrum plot over time
             sradio_col1, sradio_col2 = st.columns([0.03, 1.5])
             with sradio_col2:
-                st.session_state.spectrum = st.radio('', ['AMP Spectrum', 'RMS Spectrum'])
+                # st.session_state.spectrum = st.radio('', ['AMP Spectrum', 'RMS Spectrum'])
+                spectrum_coice = st.radio('', ['AMP Spectrum', 'RMS Spectrum'])
 
 
 
