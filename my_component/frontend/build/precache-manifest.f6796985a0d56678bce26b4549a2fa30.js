3128d12fc27732cbf429afc175ed3b9577fd0dcf
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-20 23:50:47 +0200

    revert changes

--- my_component/frontend/build/precache-manifest.f6796985a0d56678bce26b4549a2fa30.js
+++ my_component/frontend/build/precache-manifest.f6796985a0d56678bce26b4549a2fa30.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "69a8fbc427459582da925d0188a48d86",
+    "revision": "0a23a905921c4a67eeea46f0e54a9680",
     "url": "./index.html"
   },
   {
-    "revision": "083d55cb91ffcde233ad",
-    "url": "./static/js/2.08fe78c6.chunk.js"
+    "revision": "d142e371ce7e69e907fc",
+    "url": "./static/js/2.d8442821.chunk.js"
   },
   {
     "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
-    "url": "./static/js/2.08fe78c6.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.d8442821.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6c3f09946e426f0cfd7d",
-    "url": "./static/js/main.eeb5c78a.chunk.js"
+    "revision": "a94d6980d4cb177fa75f",
+    "url": "./static/js/main.172110b0.chunk.js"
   },
   {
     "revision": "7c26bca7e16783d14d15",
--- my_component/frontend/src/MyComponent.tsx
+++ my_component/frontend/src/MyComponent.tsx
@@ -3,56 +3,87 @@ import {
   StreamlitComponentBase,
   withStreamlitConnection,
 } from "streamlit-component-lib"
-
 import React, { ReactNode } from "react"
-import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
 
+interface State {
+  numClicks: number
+  isFocused: boolean
+}
 
 
 /**
  * This is a React-based component template. The `render()` function is called
  * automatically when your component should be re-rendered.
  */
+class MyComponent extends StreamlitComponentBase<State> {
+  public state = { numClicks: 0, isFocused: false }
 
-class MyComponent extends StreamlitComponentBase<any> {
-  constructor(props: any) {
-    super(props);
+  // RENDER FUNCTION
+  public render = (): ReactNode => {
 
-    this.state = {
-      recordState: null
-    };
-  }
+    // Arguments that are passed to the plugin in Python are accessible
+    // via `this.props.args`. Here, we access the "name" arg.
+    const name = this.props.args["name"]
 
-  start = () => {
-    this.setState({
-      recordState: RecordState.START
-    })
-  }
+    // Streamlit sends us a theme object via props that we can use to ensure
+    // that our component has visuals that match the active theme in a
+    // streamlit app.
+    const { theme } = this.props
+    const style: React.CSSProperties = {}
 
-  stop = () => {
-    this.setState({
-      recordState: RecordState.STOP
-    })
-  }
+    // Maintain compatibility with older versions of Streamlit that don't send
+    // a theme object.
+    if (theme) {
+      // Use the theme object to style our button border. Alternatively, the
+      // theme style is defined in CSS vars.
+      const borderStyling = `1px solid ${
+        this.state.isFocused ? theme.primaryColor : "gray"
+      }`
+      style.border = borderStyling
+      style.outline = borderStyling
+    }
 
-  //audioData contains blob and blobUrl
-  onStop = (audioData: any) => {
-    console.log('audioData', audioData)
-  }
-
-  render() {
-    const { recordState } = this.state
-
+    // Show a button and some text.
+    // When the button is clicked, we'll increment our "numClicks" state
+    // variable, and send its new value back to Streamlit, where it'll
+    // be available to the Python program.
     return (
-      <div>
-        <AudioReactRecorder state={recordState} onStop={this.onStop} />
-
-        <button onClick={this.start}>Start</button>
-        <button onClick={this.stop}>Stop</button>
-      </div>
+      <span>
+        Hello, {name}! This is a custom streamlit component! &nbsp;
+        <button
+          style={style}
+          onClick={this.onClicked}
+          disabled={this.props.disabled}
+          onFocus={this._onFocus}
+          onBlur={this._onBlur}
+        >
+          demo button
+        </button>
+      </span>
     )
   }
+
+  /** Click handler for our "Click Me!" button. */
+  private onClicked = (): void => {
+    // Increment state.numClicks, and pass the new value back to
+    // Streamlit via `Streamlit.setComponentValue`.
+    this.setState(
+      prevState => ({ numClicks: prevState.numClicks + 1 }),
+      () => Streamlit.setComponentValue(this.state.numClicks)
+    )
+  }
+
+  /** Focus handler for our "Click Me!" button. */
+  private _onFocus = (): void => {
+    this.setState({ isFocused: true })
+  }
+
+  /** Blur handler for our "Click Me!" button. */
+  private _onBlur = (): void => {
+    this.setState({ isFocused: false })
+  }
 }
+
 // "withStreamlitConnection" is a wrapper function. It bootstraps the
 // connection between your component and the Streamlit app, and handles
 // passing arguments from Python -> Component.
