3b32164cb239b714dbc7384a27cb83c8820c88d5
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-20 15:45:48 +0200

    add custom component build

---
+++ my_component/frontend/build/precache-manifest.791547612668e5f6a4620931abea9970.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4fae4efff82573c6239b69c45ca6b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d142e371ce7e69e907fc",
+    "url": "./static/js/2.d8442821.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.d8442821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ffb260391cf9c47379a",
+    "url": "./static/js/main.11d6a848.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
