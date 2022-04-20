655427da1559aa55af1fe925231050d90f9ee401
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-20 17:58:29 +0200

    random tweaks

--- my_component/frontend/build/precache-manifest.b88ce629476d10f95a732526ecf74862.js
+++ my_component/frontend/build/precache-manifest.b88ce629476d10f95a732526ecf74862.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cd862e3ca1f209db0a542ab283b177dc",
+    "revision": "8c76b774b2a4c63bef3617f09d633494",
     "url": "./index.html"
   },
   {
-    "revision": "6ff5ee6af830c4f8cc6a",
-    "url": "./static/js/2.6695de78.chunk.js"
+    "revision": "ea5c7a4cddf0cc747156",
+    "url": "./static/js/2.d04745d8.chunk.js"
   },
   {
     "revision": "ea9e8a04b4530945f0b69f09f74b01e4",
-    "url": "./static/js/2.6695de78.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.d04745d8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1c606fd632a51fe2aaa8",
-    "url": "./static/js/main.009c69af.chunk.js"
+    "revision": "07accc220c6e551500e7",
+    "url": "./static/js/main.4d52f66c.chunk.js"
   },
   {
     "revision": "7c26bca7e16783d14d15",
--- my_component/frontend/src/index.tsx
+++ my_component/frontend/src/index.tsx
@@ -1,40 +1,4 @@
 import { Streamlit, RenderData } from "streamlit-component-lib"
-import React, { Component } from 'react'
-import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
-
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      recordState: null
-    }
-  }
-  start = () => {
-    this.setState({
-      recordState: RecordState.START
-    })
-  }
-  stop = () => {
-    this.setState({
-      recordState: RecordState.STOP
-    })
-  }
-  //audioData contains blob and blobUrl
-  onStop = (audioData) => {
-    console.log('audioData', audioData)
-  }
-  render() {
-    const { recordState } = this.state
-    return (
-      <div>
-        <AudioReactRecorder state={recordState} onStop={this.onStop} />
-        <button onClick={this.start}>Start</button>
-        <button onClick={this.stop}>Stop</button>
-      </div>
-    )
-  }
-}
 
 // Add text and a button to the DOM. (You could also add these directly
 // to index.html.)
