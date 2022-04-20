57e7d5a1521d25474dfc51fbc711e221d023e6fe
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-20 17:09:22 +0200

    update path

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ import src.design as design  # design choices
 import src.detect_keyscale as detect_keyscale
 
 parent_dir = os.path.dirname(os.path.abspath(__file__))
-build_dir = os.path.join(parent_dir, "frontend/build")
+build_dir = os.path.join(parent_dir, "my_component/frontend/build")
 _component_func = components.declare_component("my_component", path=build_dir)
 
 def component_instance(name, key=None):
