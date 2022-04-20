29f666d9e91c0be8586efd55853278243b8aa08e
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-20 16:53:10 +0200

    add component info

---
+++ my_component/frontend/build/precache-manifest.b88ce629476d10f95a732526ecf74862.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c76b774b2a4c63bef3617f09d633494",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea5c7a4cddf0cc747156",
+    "url": "./static/js/2.d04745d8.chunk.js"
+  },
+  {
+    "revision": "ea9e8a04b4530945f0b69f09f74b01e4",
+    "url": "./static/js/2.d04745d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07accc220c6e551500e7",
+    "url": "./static/js/main.4d52f66c.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
