693ae47a69c4d2e1e3248c48d2f11df1f173b4c3
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-21 12:38:46 +0200

    add info

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,6 @@ import streamlit as st
 import streamlit.components.v1 as components
 # from my_component import my_component
 
-
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
-
-
 import os
 import sys
 import toml
@@ -41,44 +35,6 @@ def beatinspect_main():
 
     # DESIGN implement changes to the standard streamlit UI/UX
     design.design_setup()  # switch to primaryColor for accents
-
-
-
-    # *****************
-    stt_button = Button(label="Speak", width=100)
-
-    stt_button.js_on_event("button_click", CustomJS(code="""
-        var recognition = new webkitSpeechRecognition();
-        recognition.continuous = true;
-        recognition.interimResults = true;
-
-        recognition.onresult = function (e) {
-            var value = "";
-            for (var i = e.resultIndex; i < e.results.length; ++i) {
-                if (e.results[i].isFinal) {
-                    value += e.results[i][0].transcript;
-                }
-            }
-            if ( value != "") {
-                document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-            }
-        }
-        recognition.start();
-        """))
-
-    result = streamlit_bokeh_events(
-        stt_button,
-        events="GET_TEXT",
-        key="listen",
-        refresh_on_update=False,
-        override_height=75,
-        debounce_time=0)
-
-    if result:
-        if "GET_TEXT" in result:
-            st.write(result.get("GET_TEXT"))
-    # ****************
-
 
     # TITLE and Information
     header_col1, header_col2, header_col3 = st.columns([10, 2.5, 2.5])
