60e3c4761beefb498eda623e9ce79129e4f95bad
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-21 12:17:32 +0200

    add bokeh event code test

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,13 @@ import streamlit as st
 import streamlit.components.v1 as components
 # from my_component import my_component
 
+
+import streamlit as st
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+
+
 import os
 import sys
 import toml
@@ -67,9 +74,44 @@ def beatinspect_main():
                 st.write('')  # ad spacing
                 st.write('SOON to be implemented!')
 
-                with st.spinner('servus'):
-                    component_returnvalue = _component_func()
-                    st.write(component_returnvalue)
+                # with st.spinner('servus'):
+                #     component_returnvalue = _component_func()
+                #     st.write(component_returnvalue)
+
+
+
+                stt_button = Button(label="Speak", width=100)
+
+                stt_button.js_on_event("button_click", CustomJS(code="""
+                var recognition = new webkitSpeechRecognition();
+                recognition.continuous = true;
+                recognition.interimResults = true;
+
+                recognition.onresult = function (e) {
+                    var value = "";
+                    for (var i = e.resultIndex; i < e.results.length; ++i) {
+                        if (e.results[i].isFinal) {
+                            value += e.results[i][0].transcript;
+                        }
+                    }
+                    if ( value != "") {
+                        document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+                    }
+                }
+                recognition.start();
+                """))
+
+                result = streamlit_bokeh_events(
+                    stt_button,
+                    events="GET_TEXT",
+                    key="listen",
+                    refresh_on_update=False,
+                    override_height=75,
+                    debounce_time=0)
+
+                if result:
+                    if "GET_TEXT" in result:
+                        st.write(result.get("GET_TEXT"))
 
                 # https://www.youtube.com/watch?v=BuD3gILJW-Q&ab_channel=Streamlit
                 # USE streamlit custom component that implements a custom html/css/react element
