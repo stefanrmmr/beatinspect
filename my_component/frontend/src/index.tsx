cd28b0771d09e65d9b29ed4774011c3ee2309e72
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-21 16:37:38 +0200

    add

--- my_component/frontend/src/index.tsx
+++ my_component/frontend/src/index.tsx
@@ -30,6 +30,55 @@ button_rec.onblur = function(): void {
   isFocused = false
 }
 
+//*********
+var canvas = document.querySelector("canvas");
+
+// Optional frames per second argument.
+var stream = canvas.captureStream(25);
+var recordedChunks = [];
+
+console.log(stream);
+var options = { mimeType: "video/webm; codecs=vp9" };
+mediaRecorder = new MediaRecorder(stream, options);
+
+mediaRecorder.ondataavailable = handleDataAvailable;
+mediaRecorder.start();
+
+function handleDataAvailable(event) {
+  console.log("data-available");
+  if (event.data.size > 0) {
+    recordedChunks.push(event.data);
+    console.log(recordedChunks);
+    download();
+  } else {
+    // ...
+  }
+}
+function download() {
+  var blob = new Blob(recordedChunks, {
+    type: "video/webm"
+  });
+  var url = URL.createObjectURL(blob);
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.href = url;
+  a.download = "test.webm";
+  a.click();
+  window.URL.revokeObjectURL(url);
+}
+
+// demo: to download after 5sec
+setTimeout(event => {
+  console.log("stopping");
+  mediaRecorder.stop();
+}, 5000);
+//************
+
+
+
+
+
 /**
  * The component's render function. This will be called immediately after
  * the component is initially loaded, and then again every time the
