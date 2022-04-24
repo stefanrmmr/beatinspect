cc2a3ad91a60a8981f1bfce9c95e5a46ef4af932
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-04-24 18:58:27 +0200

    styles

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,7 +112,7 @@ def beatinspect_main():
 
                 rec_msg = '<p style="color: #e3fc03; font-size: 1rem;">After clicking this button you will automatically be recorded for 8 seconds!</p>'
                 st.markdown(rec_msg, unsafe_allow_html=True)
-                stt_button  = Button(label="Start Recording", width=120, height=40, margin=(0, 0, 0, 0))
+                stt_button  = Button(label="Start Recording", width=120, height=40, margin=(10, 10, 10, 10))
 
                 stt_button.js_on_event("button_click", CustomJS(code="""
                 const timeMilliSec = 8000 //Fixed 10sec recording
