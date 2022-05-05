5e8a9f76b2238a8e3391d0b5baa61c3a76553bda
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-05-05 04:41:38 +0200

    add insta link to UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,8 +47,9 @@ def beatinspect_main():
     header_col1, header_col2, header_col3 = st.columns([10, 2.5, 2.5])
     with header_col1:
         st.title('beat inspect ™')
-        st.markdown('Version 1.3.2 - April 2022 - Github '
-            '[@stefanrmmr](https://github.com/stefanrmmr/beatinspect)')
+        st.markdown('Version 1.3.2 - April 2022 - '
+            '[GitHub](https://github.com/stefanrmmr/beatinspect) - '
+            '[Instagram](https://www.instagram.com/beatinspect/)'))
     with header_col3:
         st.write('')  # add spacing
         st.image("img/rs_logo_transparent_yellow.png")
