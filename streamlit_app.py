9dbcd067b455b1370fba53a8a5e0e3a40b828402
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-05-09 18:01:25 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,8 +96,9 @@ def beatinspect_main():
                     base64data_audio = base64data_audio.replace('data:audio/wav;base64,', '')
                     st.write(base64data_audio)
 
-                    with open("temp.wav", "wb") as fh:
-                        fh.write(base64.decodebytes(base64data_audio))
+                    wav_file = open("temp.wav", "wb")
+                    decode_string = base64.b64decode(base64data_audio)
+                    wav_file.write(decode_string)
 
                     audiofile_path = os.path.join(os.getcwd(), "temp.wav")
                     st.audio(audiofile_path)
