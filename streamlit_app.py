2a2b7b747069dc014c727cbc3cf3687cd6ce7ff7
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-05-16 02:16:03 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,7 +80,7 @@ def beatinspect_main():
             elif 'Record' in choice:
                 audiofile = None
 
-                rec_msg = '<p style="color: #e3fc03; font-size: 1rem;">Use this audio-recorder to generate files that can be analyzed using beatinspect! <br/> Record at least 15 seconds of audio for optimal functionality!</p>'
+                rec_msg = '<p style="color: #e3fc03; font-size: 1rem;">Use this audio-recorder to generate files that can be analyzed using beatinspect! (at least 15 seconds for optimal functionality)</p>'
                 st.markdown(rec_msg, unsafe_allow_html=True)
 
                 # the audiorec custom component
