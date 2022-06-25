711feaf7c6c095847bc7a5087cfac69dbedb3925
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-06-25 18:34:13 +0200

    plotly horizontal colorbar

--- src/plots.py
+++ src/plots.py
@@ -332,5 +332,6 @@ def melspectrogram_plotly3d(y, sr):
 
     # define colorbar descriptive title
     fig.data[0].colorbar.title = "Amplitude [dB]"
+    fig.update_coloraxes(colorbar_orientation="h")  # instead of vertical
 
     st.plotly_chart(fig, use_container_width=True, config=config)
