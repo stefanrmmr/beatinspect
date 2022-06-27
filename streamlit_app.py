b274c1be5fa46e29255a5d251f36913936e3a0df
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-06-27 14:31:36 +0200

    fix slider right margin

--- streamlit_app.py
+++ streamlit_app.py
@@ -240,7 +240,7 @@ def beatinspect_main():
 
                 # radio button selection for spectrum plot over time
                 streamlit_design.radiobutton_horizontal()  # switch alignment
-                sradio1_col1, sradio1_col2, sradio1_col3 = st.columns([0.04, 1.5, 1])
+                sradio1_col1, sradio1_col2, sradio1_col3, sradio_col4 = st.columns([0.04, 1.5, 1.5, 0.1])
                 with sradio1_col2:
                     st.session_state.spectrum3d = st.radio('Please select your prefered Mel-Spectrum viewing mode.',
                                                          ['Default Top View  ', 'Peaks Detection  '])
