41d397b41dfc7e35d7261466f4a1772d73e7e1df
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-06-28 23:14:02 +0200

    add paddign to slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -259,11 +259,13 @@ def beatinspect_main():
                 #      otherwise use the -25% on both ends approach as listed below
 
                 # AUDIO TIMEFRAME Selection for Mel-Spectrogram
-                streamlit_design.add_spacing(3)  # add linebreaks
-                sec_range = st.slider('Select Timeframe for the spectrogram (limited performance if larger than 30sec)',
-                                      0, int(duration), (int(duration*0.25), int(duration*0.75)))
-                y_slice, sr_slice = librosa.load(audiofile_path, sr=sampling_freq, offset=sec_range[0],
-                                                 duration=sec_range[1] - sec_range[0])
+                slider0_col1, slider0_col2, slider0_col3 = st.columns([0.1, 3.0, 0.1])
+                with slider0_col2:  # add columns for sufficient padding 
+                    streamlit_design.add_spacing(3)  # add linebreaks
+                    sec_range = st.slider('Select Timeframe for the spectrogram (limited performance if larger than 30sec)',
+                                          0, int(duration), (int(duration*0.25), int(duration*0.75)))
+                    y_slice, sr_slice = librosa.load(audiofile_path, sr=sampling_freq, offset=sec_range[0],
+                                                     duration=sec_range[1] - sec_range[0])
 
 
                 fullscreen_msg = '<p style="color: #e3fc03; font-size: 1rem;">'\
